746db1ff9280a66b6c217b927811e1d55b707ef6
BirajAd <adhikari.biraj1@gmail.com>
2023-02-23 22:49:17 -0600

    traffic assignment accuracy improved to be 98%

--- traffic/recognition.py
+++ traffic/recognition.py
@@ -3,8 +3,6 @@ import numpy as np
 import cv2
 import sys
 
-signDict = {0: 'stop', 1: 'straight or left', 2:'speed limit'}
-
 def main():
     if(len(sys.argv) != 3):
         sys.exit("Usage: python recognition.py model_directory image")
@@ -20,13 +18,13 @@ def load_saved_model(folder):
 #recognize image with the trained model
 def recognize_image(theModel, image):
     img = cv2.imread(image)
-    img.resize(30,30,3)
-    img = img/255
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    img = tf.image.resize(img, (30,30))
     ans = load_saved_model(theModel)
-    ans.summary()
-    arr = ans.predict([np.array(img).reshape(1,30,30,3)])
-    print([float(i) for i in arr[0]])
-    return signDict[ans.predict([np.array(img).reshape(1,30,30,3)]).argmax()]
+    # ans.summary()
+    # arr = ans.predict([np.array(img).reshape(1,30,30,3)])
+    # print([float(i) for i in arr[0]]
+    return ans.predict([np.array(img).reshape(1,30,30,3)]).argmax()
 
 if __name__ == "__main__":
     main()
